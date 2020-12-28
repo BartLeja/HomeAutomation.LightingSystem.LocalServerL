2323aefa763c754b7c12cbecd562b595ae65cdc5
Bartłomiej Leja <bartomiejleja@gmail.com>
2020-12-28 10:00:53 +0100

    Removed forecastWeather

---
+++ HomeAutomation.LightingSystem.LocalServiceL/Controllers/LightingSystemLocalController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HomeAutomation.LightingSystem.LocalServiceL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LightingSystemLocalController : ControllerBase
+    {
+        private readonly ILogger<LightingSystemLocalController> _logger;
+
+        public LightingSystemLocalController(ILogger<LightingSystemLocalController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+           return "Lighting System Local -> Works";
+        }
+    }
+}
